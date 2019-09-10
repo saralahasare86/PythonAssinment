071c1d02faae8fc52d9fa20310425de041edc25d
unknown <saralahasare86@gmail.com>
2019-09-10 16:49:16 +0530

    assignment1 file is added

---
+++ assignment1.py
@@ -0,0 +1,14 @@
+s='python for life'
+#print(s)
+size=len(s)
+print(size)
+print(s[0:6])
+print(s[7:10])
+print(s[11:15])
+print(s[2:10])
+print(s[8:13])
+
+#print(s[0:11]+' '+ s[11:15].upper())
+print(s[0:11]+ s[11:15].upper())
+#print(s[0:6].upper()+' '+ s[7:10]+' '+ s[11:15].upper())
+print(s[0:7].upper()+ s[7:10]+' '+ s[11:15].upper())
